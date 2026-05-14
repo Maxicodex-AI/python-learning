d32b293e4473c891f152b58dbf1795669878ccfa
Maxi <chienyimax911@gmail.com>
2026-05-14 13:47:39 +0100

    Day 1 (print, strings, numbers, comments)

---
+++ day1.py
@@ -0,0 +1,24 @@
+# =====================================
+# DAY 1 PROGRAM
+# =====================================
+
+"""
+This program asks the user to enter:
+- Name
+- School
+- Department
+- Dream career
+"""
+
+# Input section
+name = input("Enter your name: ")
+school_name = input("Enter the name of your school: ")
+department = input("Enter your department: ")
+dream_career = input("Enter your dream career: ")
+
+# Output section
+print("\n===== USER DETAILS =====")
+print("Name:", name)
+print("School:", school_name)
+print("Department:", department)
+print("Dream Career:", dream_career)
